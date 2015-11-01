eda1c83800c0a3f3873bad2361931bbde6854b18
smartytwiti <marwen109@gmail.com>
2015-11-01 15:32:37 +0100

    add signup feature - UI

--- frontend/modules/login/LoginController.js
+++ frontend/modules/login/LoginController.js
@@ -1,10 +1,26 @@
 var loginModule = angular.module('module.login', []);
 
-loginModule.controller('LoginController', function($scope,$rootScope,$state) {
+loginModule.controller('LoginController', function($scope,$rootScope,$state,$http) {
+
+
+	$scope.signupData = {
+		username: '',
+		password: ''
+	};
 
     $scope.signin = function() {
       $rootScope.nickname=$scope.nickname;
       $state.go('chat');
     };
+
+
+  	$scope.signup = function() {
+	  	$http.post('/api/user',{
+	  		username : $scope.signupData.username,
+	  		password: $scope.signupData.password
+	  	}).success(function(data){
+	  		$scope.nickname = data.username;
+	  	});
+  	};
 
 });
